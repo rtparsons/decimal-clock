f37e23db1a15132f01ebae9a1f76b39300928a90
rtparsons <rtparsons@hotmail.co.uk>
2021-09-21 09:38:09 +0100

    Increase Jay factor

--- src/clocks/JayTimeDisplay.tsx
+++ src/clocks/JayTimeDisplay.tsx
@@ -2,7 +2,8 @@ function JayTimeDisplay(props: any) {
     let date: Date = props.date;
     const secondsInDay = (24 * 60 * 60 );
     const secondsSoFar = (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds()); 
-    const msOffset = (secondsInDay - secondsSoFar) * 6.6;
+
+    const msOffset = (secondsInDay - secondsSoFar) * 6.9;
     date =  new Date(date.getTime() - msOffset);
     const hours = padWithZero(date.getHours());
     const mins = padWithZero(date.getMinutes());
