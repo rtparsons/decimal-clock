650d25e0981dc51af3d8febcafe6f0f6f71caed7
rtparsons <rtparsons@hotmail.co.uk>
2025-02-23 11:44:35 +0000

    Add ThemeProvider to App component for Material-UI theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Dashboard from './Dashboard';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./Dashboard";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme();
 
 export default class App extends React.Component {
-
-    render() {
-        return (
-            <BrowserRouter>
-                <Dashboard></Dashboard>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Dashboard></Dashboard>
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+  }
 }
